4a4f4a3f855f42cab6e8d7272cce395b9f5ed726
cr9co0 <alburton32@gmail.com>
2025-04-05 13:32:11 +0500

    4.4.1 Setting Configuration Settings in the Application
    Create module accessor for author_class
    
    Switch the Blorgh::Article model over to this new setting
    
    Update set_author method
    
    Update author_class

--- app/models/blorgh/article.rb
+++ app/models/blorgh/article.rb
@@ -2,13 +2,18 @@ module Blorgh
   class Article < ApplicationRecord
     attr_accessor :author_name
     has_many :comments
-    belongs_to :author, class_name: "User"
+    belongs_to :author, class_name: Blorg.author_class
 
     before_validation :set_author
 
     private
       def set_author
         self.author = User.find_or_create_by(name: author_name)
+        self.author = Blorgh.author_class.find_or_create_by(name: author_name)
+      end
+
+      def self.author_class
+        @@author_class.constantize
       end
   end
 end
